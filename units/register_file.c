0f74b59182db0d5ece663cd139822a01333e83d3
thunder04 <70254851+thunder04@users.noreply.github.com>
2024-02-08 11:39:08 +0200

    Add validation and debug messages to register file

--- units/register_file.c
+++ units/register_file.c
@@ -1,4 +1,13 @@
 #include "register_file.h"
+#include "../macros.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+#define VALIDATE(REG)                                                          \
+  if (REG > 31) {                                                              \
+    printf(#REG " cannot exceed 31. It was: %u\n", REG);                       \
+    exit(1);                                                                   \
+  }
 
 // Register File
 static unsigned int REG[32] = {0};
@@ -9,8 +18,24 @@ struct RegOut reg(unsigned int ReadRegister1, unsigned int ReadRegister2,
   struct RegOut regOut = {0, 0};
 
   if (RegWrite) {
+    VALIDATE(WriteRegister);
+
+#ifdef DEBUG
+    printf(ANSI_FM "[DEBUG] W REG[%u] <== %u\n" ANSI_0, WriteRegister,
+           WriteData);
+#endif
+
     REG[WriteRegister] = WriteData;
   } else {
+    VALIDATE(ReadRegister1);
+    VALIDATE(ReadRegister2);
+
+#ifdef DEBUG
+    printf(
+        ANSI_FM "[DEBUG] R RD1: REG[%u] ==> %u\tRD2: REG[%u] ==> %u\n" ANSI_0,
+        ReadRegister1, REG[ReadRegister1], ReadRegister2, REG[ReadRegister2]);
+#endif
+
     regOut.ReadData1 = REG[ReadRegister1];
     regOut.ReadData2 = REG[ReadRegister2];
   }
